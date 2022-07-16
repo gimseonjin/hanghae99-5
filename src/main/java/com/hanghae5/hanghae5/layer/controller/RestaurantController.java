584d1241e25a075fc30775726f1b83a7dfc72427
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2022-07-16 17:50:51 +0900

    Feature : Create Restaurant Controller

--- src/main/java/com/hanghae5/hanghae5/layer/controller/RestaurantController.java
+++ src/main/java/com/hanghae5/hanghae5/layer/controller/RestaurantController.java
@@ -1,18 +1,19 @@
 package com.hanghae5.hanghae5.layer.controller;
 
+import com.hanghae5.hanghae5.layer.model.Food;
+import com.hanghae5.hanghae5.layer.model.Restaurant;
+import com.hanghae5.hanghae5.layer.model.dto.request.AddMenuRequest;
 import com.hanghae5.hanghae5.layer.model.dto.request.CreateRestaurantRequest;
 import com.hanghae5.hanghae5.layer.service.RestaurantService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
-@RequestMapping("/restaurant")
+@RequestMapping("")
 public class RestaurantController {
 
     private final RestaurantService restaurantService;
@@ -21,8 +22,29 @@ public class RestaurantController {
         this.restaurantService = restaurantService;
     }
 
-    @PostMapping("/register")
+    @PostMapping("/restaurant/register")
     public ResponseEntity register(@RequestBody @Valid CreateRestaurantRequest createRestaurantRequest){
+        restaurantService.addRestaurant(createRestaurantRequest);
         return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/restaurants")
+    public ResponseEntity getRestaurant(){
+        List<Restaurant> results = restaurantService.getAllRestaurant();
+        return new ResponseEntity(results, HttpStatus.OK);
+    }
+
+    @PostMapping("/restaurant/{restaurantId}/food/register")
+    public ResponseEntity foodRegister(
+            @RequestBody @Valid List<AddMenuRequest> addMenuRequestList, @PathVariable Long restaurantId){
+        restaurantService.addMenus(restaurantId, addMenuRequestList);
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/restaurant/{restaurantId}/foods")
+    public ResponseEntity getMenus(
+            @PathVariable Long restaurantId){
+        List<Food> results = restaurantService.getMenus(restaurantId);
+        return new ResponseEntity(results, HttpStatus.CREATED);
     }
 }
